a4ca2e66d8d1963d633b75787569cee4f645a2f5
kanada4310 <gaimon.maam@gmail.com>
2025-07-06 09:30:58 +0900

    fix(lint): Format src/i18n/index.ts again

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,8 +2,8 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import deTranslation from "./locales/de/translation.json";
 import enTranslation from "./locales/en/translation.json";
-import svTranslation from "./locales/sv/translation.json";
 import jaTranslation from "./locales/ja/translation.json";
+import svTranslation from "./locales/sv/translation.json";
 
 const resources = {
   en: {
@@ -20,15 +20,13 @@ const resources = {
   },
 };
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources,
-    lng: "ja",
-    fallbackLng: "en",
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "ja",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
