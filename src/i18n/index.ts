4b0f9beca3c1f9ef72240b538ec00ea440922701
kanada4310 <gaimon.maam@gmail.com>
2025-07-06 09:24:05 +0900

    fix(typecheck): Remove comments from src/i18n/index.ts to resolve TS errors

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,12 +2,9 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import deTranslation from "./locales/de/translation.json";
 import enTranslation from "./locales/en/translation.json";
-import jaTranslation from "./locales/ja/translation.json";
 import svTranslation from "./locales/sv/translation.json";
+import jaTranslation from "./locales/ja/translation.json";
 
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
   en: {
     translation: enTranslation,
@@ -24,15 +21,13 @@ const resources = {
 };
 
 i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(initReactI18next)
   .init({
     resources,
-    lng: "ja", // 変更
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-    fallbackLng: "en", // 変更
+    lng: "ja",
+    fallbackLng: "en",
     interpolation: {
-      escapeValue: false, // react already safes from xss
+      escapeValue: false,
     },
   });
 
